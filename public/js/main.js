5e02bec0afcfc300704cc2c731e92b360ec40e5b
adi <adij82@gmail.com>
2018-10-14 09:58:23 +0300

    Update versions

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,3 @@
 $(document).ready(function () {
 
-  // var dataString = JSON.parse('{"filter":[{"left":"change","operation":"nempty"},{"left":"sector","operation":"in_range","right":["Major","Minor"]}],"symbols":{"query":{"types":["forex"]}},"columns":["name","close","change","change_abs","sector","country","bid","ask","high","low","Recommend.All","description","name","subtype","pricescale","minmov","fractional","minmove2"],"sort":{"sortBy":"change","sortOrder":"desc"},"options":{"lang":"en"},"range":[0,150]}');
-  //
-  // $.ajax({
-  //   type: "post",
-  //   url: "https://scanner.tradingview.com/forex/scan",
-  //   data: dataString,
-  //   dataType : "json",
-  //   contentType: "application/json; charset=utf-8",
-  //   error: function (err) {
-  //     console.info('error: ' + err);
-  //   },
-  //   success: function (data) {
-  //     console.info('Yay! ' + data);
-  //   }
-  //
-  // });
-  //
-  // console.log(dataString)
 });
